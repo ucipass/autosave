96e601762e477410b2ae7593fc5e46a3d505c242
ucipass <ucipass@gmail.com>
2018-12-03 16:57:25 -0600

    distribution

--- autosave.py
+++ autosave.py
@@ -1,5 +1,6 @@
 from netmiko import Netmiko
 from getpass import getpass
+import textfsm
 import re
 import yaml
 import sys
@@ -7,16 +8,82 @@ import csv
 import time
 import os
 import argparse
+import io
 
 dir_root = os.path.dirname(os.path.realpath(__file__))
 dir_fsm = os.path.join(dir_root, 'templates')
-os.environ['NET_TEXTFSM'] = dir_fsm
 inventoryFile = "inventory.yml"
 inventory = { "cisco_ios" : [], 'cisco_nxos' : []}
-arp = []
-mac = []
 
+cisco_ios_show_mac_address_table = """Value DESTINATION_ADDRESS (\w+.\w+.\w+)
+Value TYPE (\w+)
+Value VLAN (\w+)
+Value DESTINATION_PORT (\S+)
 
+Start
+  ^Destination\s+Address\s+Address\s+Type\s+VLAN\s+Destination\s+Port -> TYPE1
+  ^\s+vlan\s+mac address\s+type\s+learn\s+age\s+ports -> TYPE2
+  ^\s+vlan\s+mac address\s+type\s+protocols\s+port -> TYPE3
+  ^Vlan\s+Mac Address\s+Type\s+Ports -> TYPE4
+
+TYPE1
+  ^${DESTINATION_ADDRESS}\s+${TYPE}\s+${VLAN}\s+${DESTINATION_PORT} -> Record
+
+TYPE2
+  ^[\*|\s]\s+${VLAN}\s+${DESTINATION_ADDRESS}\s+${TYPE}\s+\S+\s+\S+\s+${DESTINATION_PORT} -> Record
+
+TYPE3
+  ^\s+${VLAN}\s+${DESTINATION_ADDRESS}\s+${TYPE}\s+\S+\s+${DESTINATION_PORT} -> Record
+
+TYPE4
+  ^\s+${VLAN}\s+${DESTINATION_ADDRESS}\s+${TYPE}\s+${DESTINATION_PORT} -> Record
+"""
+cisco_ios_show_ip_arp = """Value Required ADDRESS (\d+\.\d+\.\d+\.\d+)
+Value Required AGE ([-\d+]+)
+Value Required MAC (\S+)
+Value Required TYPE (\S+)
+Value INTERFACE (\S+)
+
+Start
+  ^Internet\s+${ADDRESS}\s+${AGE}\s+${MAC}\s+${TYPE}\s+${INTERFACE} -> Record
+  ^Internet\s+${ADDRESS}\s+${AGE}\s+${MAC}\s+${TYPE} -> Record
+"""
+cisco_nxos_show_mac_address_table = """Value VLAN (\S+)
+Value MAC (\S+)
+Value TYPE (\S+)
+Value AGE (\S+)
+Value SECURE ([TF])
+Value NTFY ([TF])
+Value PORTS (\S+)
+
+Start
+  ^VLAN\s+MAC\s+Address\s+Type\s+age\s+Secure\s+NTFY\s+Ports -> Continue
+  ^.*\s${VLAN}\s+${MAC}\s+${TYPE}\s+${AGE}\s+${SECURE}\s+${NTFY}\s+${PORTS} -> Record
+"""
+cisco_nxos_show_ip_arp = """Value Required ADDRESS (\d+\.\d+\.\d+\.\d+)
+Value Required AGE (\S+)
+Value Required MAC (\S+)
+Value INTERFACE (\S+)
+
+Start
+  ^Address\s+Age\s+MAC Address\s+Interface -> Start_record
+
+Start_record
+  ^${ADDRESS}\s+${AGE}\s+${MAC}\s+${INTERFACE} -> Record
+  ^\s+$$
+  ^$$
+  ^.* -> Error "LINE NOT FOUND"
+"""
+fsm = {
+    'cisco_ios': {
+        'arp': cisco_ios_show_ip_arp,
+        'mac': cisco_ios_show_mac_address_table
+    },
+    'cisco_nxos': {
+        'arp': cisco_nxos_show_ip_arp,
+        'mac': cisco_nxos_show_mac_address_table
+    }
+}
 
 def sshVerify(host, username, password, device_type):
     try:
@@ -38,89 +105,86 @@ def sshVerifyInventory(device_type):
 
 def sshGetArp(device_type):
     global inventory
-    global arp
+    table_all = []
     dir = os.path.dirname(os.path.realpath(__file__))
     dir = os.path.join(dir, 'arp')
     if not os.path.exists(dir):
         os.makedirs(dir)
-    header = ['address', 'mac', 'host', 'interface', 'age', 'type']
-    try:
-        iterator = iter(inventory[device_type])
-    except Exception as e:
-        return False #no good yaml content found
+    #fsm_file = os.path.join(dir_fsm, device_type+"_show_ip_arp.template")
+    #re_table = textfsm.TextFSM(open(fsm_file, 'r'))
+
     for device in inventory[device_type]:
-        host_arp = []
-        host=device['host']
-        username=device['username']
-        password=device['password']
+        table = []
+        host = device['host']
+        username = device['username']
+        password = device['password']
         try:
             print("Retrieving ARP table for:", host)
             net_connect = Netmiko(host=host, username=username, password=password, device_type=device_type, timeout=10)
             hostname = net_connect.find_prompt()
-            hostname = re.sub('\W+','', hostname)
-            cmd_output = net_connect.send_command("term len 0", use_textfsm=False)
-            cmd_output = net_connect.send_command("show ip arp", use_textfsm=True)
+            hostname = re.sub('#','', hostname)
+            net_connect.send_command("term len 0", use_textfsm=False)
+            cmd_output = net_connect.send_command("show ip arp", use_textfsm=False)
+            re_table = textfsm.TextFSM(io.StringIO(fsm[device_type]['arp']))
+            cmd_output = re_table.ParseText(cmd_output)
+
             if not "ailed" in cmd_output:
-                print(" ",cmd_output)
-                for item in cmd_output:
-                    arpEntry = [item['address'], item['mac'], host, item['interface'], item['age'], item['type']]
-                    host_arp.append(arpEntry)
-                host_arp = sorted(host_arp, key=None, reverse=False)
-                timestr = time.strftime("%Y%m%d-%H%M%S")
-                filename = os.path.join(dir, hostname + "_ARP_" + timestr + ".csv" )
-                writeCSV(header, host_arp, filename)
+                print(" Success!: ", cmd_output[0], "more...")
+                for arpEntry in cmd_output:
+                    table.append(arpEntry)
+                table = sorted(table, key=None, reverse=False)
+                filename = os.path.join(dir, hostname + "_arp_" + time.strftime("%Y%m%d-%H%M%S") + ".csv")
+                writeCSV(re_table.header, table, filename)
             else:
-                print("Failed:\n", cmd_output)
+                print(" Failed!:\n", cmd_output)
         except Exception as e:
             print(e)
-        arp = arp + host_arp
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    filename = os.path.join(dir, "ALL_ARP_" + timestr + ".csv")
-    arp = sorted(arp, key=None, reverse=False)
-    writeCSV(header,arp,filename)
+        table_all = table_all + table
+    filename = os.path.join(dir, "all_arp_" + time.strftime("%Y%m%d-%H%M%S") + ".csv")
+    table_all = sorted(table_all, key=None, reverse=False)
+    writeCSV(re_table.header, table_all, filename)
+
 
 def sshGetMac(device_type):
     global inventory
-    global mac
+    table_all = []
     dir = os.path.dirname(os.path.realpath(__file__))
     dir = os.path.join(dir, 'mac')
     if not os.path.exists(dir):
         os.makedirs(dir)
-    header = ['vlan','destination_address', 'destination_port', 'type', 'host']
-    try:
-        iterator = iter(inventory[device_type])
-    except Exception as e:
-        return False #no good yaml content found
+    #fsm_file = os.path.join(dir_fsm, device_type+"_show_ip_arp.template")
+    #re_table = textfsm.TextFSM(open(fsm_file, 'r'))
+
     for device in inventory[device_type]:
-        host_mac = []
-        host=device['host']
-        username=device['username']
-        password=device['password']
+        table = []
+        host = device['host']
+        username = device['username']
+        password = device['password']
         try:
             print("Retrieving MAC table for:", host)
             net_connect = Netmiko(host=host, username=username, password=password, device_type=device_type, timeout=10)
             hostname = net_connect.find_prompt()
-            hostname = re.sub('#','', hostname )
-            cmd_output = net_connect.send_command("term len 0", use_textfsm=False)
-            cmd_output = net_connect.send_command("show mac address-table", use_textfsm=True)
-            if not 'nvalid' in cmd_output:
-                print(" ",cmd_output)
-                for item in cmd_output:
-                    macEntry = [item['vlan'], item['destination_address'], item['destination_port'], item['type'], host]
-                    host_mac.append(macEntry)
-                host_mac = sorted(host_mac, key=None, reverse=False)
-                timestr = time.strftime("%Y%m%d-%H%M%S")
-                filename = os.path.join(dir, hostname + "_MAC_" + timestr + ".csv")
-                writeCSV(header, host_mac, filename)
+            hostname = re.sub('#','', hostname)
+            net_connect.send_command("term len 0", use_textfsm=False)
+            cmd_output = net_connect.send_command("show mac address-table", use_textfsm=False)
+            re_table = textfsm.TextFSM(io.StringIO(fsm[device_type]['mac']))
+            cmd_output = re_table.ParseText(cmd_output)
+
+            if not "ailed" in cmd_output:
+                print(" Success!: ", cmd_output[0], "more...")
+                for arpEntry in cmd_output:
+                    table.append(arpEntry)
+                table = sorted(table, key=None, reverse=False)
+                filename = os.path.join(dir, hostname + "_mac_" + time.strftime("%Y%m%d-%H%M%S") + ".csv")
+                writeCSV(re_table.header, table, filename)
             else:
-                print("  Failed to retrieve MAC table for: ", host)
+                print(" Failed!:\n", cmd_output)
         except Exception as e:
             print(e)
-        mac = mac + host_mac
-    mac = sorted(mac, key=None, reverse=False)
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    filename = os.path.join(dir, "ALL_MAC_" + timestr + ".csv")
-    writeCSV(header,mac,filename)
+        table_all = table_all + table
+    filename = os.path.join(dir, "all_mac_" + time.strftime("%Y%m%d-%H%M%S") + ".csv")
+    table_all = sorted(table_all, key=None, reverse=False)
+    writeCSV(re_table.header, table_all, filename)
 
 def sshShowRun(device_type):
     global inventory
